914449cb75417a9ad8ace64709fc87c01084c128
autumn <1060590974@qq.com>
2025-09-28 15:03:50 +0800

    优化分类

---
+++ yolo/train.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO(r"D:\autumn\Documents\GitHub\ultralytics\ultralytics\yolo_workspace\train_output\bird\train\train3\weights\last.pt")
+    # model = YOLO("yolo11s.yaml").load("yolo11s.pt")
+    # model = YOLO("yolo11n.pt")
+    # model = YOLO("yolo11s.yaml")
+
+    # Train the model
+    model.train(
+        data=r"D:\autumn\Downloads\bird.v6i.yolov11\data.yaml",
+        epochs=100,
+        batch=48,
+        imgsz=640,
+        workers=8,
+        resume=True, #从上次保存的检查点恢复训练
+        save_period=10, #保存间隔
+        project="train_output/bird/train",
+        # name="train"
+    )
