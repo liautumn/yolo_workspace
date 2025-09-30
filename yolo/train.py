c6a87a378f2d59a95f52087e414b8b3afd998b2f
autumn <1060590974@qq.com>
2025-09-30 08:42:41 +0800

    bird02

--- yolo/train.py
+++ yolo/train.py
@@ -2,20 +2,20 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO(r"D:\autumn\Documents\GitHub\ultralytics\ultralytics\yolo_workspace\train_output\bird\train\train3\weights\last.pt")
+    # model = YOLO(r"D:\autumn\Documents\GitHub\ultralytics\ultralytics\yolo_workspace\train_output\bird\train\train3\weights\last.pt")
     # model = YOLO("yolo11s.yaml").load("yolo11s.pt")
-    # model = YOLO("yolo11n.pt")
+    model = YOLO("yolo11s.pt")
     # model = YOLO("yolo11s.yaml")
 
     # Train the model
     model.train(
         data=r"D:\autumn\Downloads\bird.v6i.yolov11\data.yaml",
-        epochs=100,
-        batch=48,
+        epochs=250,
+        batch=32,
         imgsz=640,
         workers=8,
-        resume=True, #从上次保存的检查点恢复训练
+        resume=False, #从上次保存的检查点恢复训练
         save_period=10, #保存间隔
-        project="train_output/bird/train",
-        # name="train"
+        project="../train_output/bird/train",
+        name="train02"
     )
