7f6b7fb7784fcff5a0af1dda14fcb22aa7f59fe3
autumn <1060590974@qq.com>
2026-03-13 22:04:43 +0800

    1

--- model_export/rknn/rknn_export.py
+++ model_export/rknn/rknn_export.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 
     # Load a model
-    model = YOLO(r"D:\autumn\Documents\GitHub\ultralytics\ultralytics\yolo_workspace\鸟\训练\train2\weights\best.pt")  # load an official model
+    model = YOLO(r"yolo11s")  # load an official model
 
     # Export the model 导出到 RKNN 时，请确保使用 x86 Linux 机器
     # RKNN ===> imgsz(h,w), batch, name, device
